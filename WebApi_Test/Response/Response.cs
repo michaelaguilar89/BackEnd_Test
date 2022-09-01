262c0bd6eb649c9a250cb48d6faa05a408bc40f3
Michael_Aguilar <mchlaguilar051@hotmail.es>
2022-08-31 18:59:24 -0600

    agregando clase response

---
+++ WebApi_Test/Response/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi_Test.Response
+{
+    public class Response
+    {
+        public Object Result { get; set; }
+
+        public String  DisplayMessages { get; set; }
+        public String ErrorMessages { get; set; }
+
+        public bool IsSucces { get; set; }
+    }
+}
